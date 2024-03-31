87f6c9d42f1cc8c77c8bdb398938648f150db4a8
Avdeykina Valeriya <1397-avp21u792@users.noreply.git.iu7.bmstu.ru>
2024-04-01 00:19:52 +0300

    add test project

---
+++ src/test/hello/src/main/c/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int (*f) (const int);
+
+int square(const int x)
+{
+    return x * x;
+}
+
+int memoize(f func, const int arg)
+{
+    
+}
+
+int main()
+{
+
+
+    return 0;
+}
