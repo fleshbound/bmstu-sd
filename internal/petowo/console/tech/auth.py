e77d761b677eac19bbfc095ac4300314830c5749
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-08-28 12:04:08 +0300

    add console select

--- internal/petowo/console/tech/auth.py
+++ internal/petowo/console/tech/auth.py
@@ -1,6 +1,22 @@
+from core.auth.schema.auth import Token
+from internal.petowo.console.tech.console import UserConsoleInfo
+
+
 class AuthHandler:
     def verify_auth_role(self):
         raise NotImplementedError
 
     def verify_auth(self):
+        raise NotImplementedError
+
+    def signin(self) -> UserConsoleInfo:
+        raise NotImplementedError
+
+    def verify_token(self, token: Token) -> bool:
+        raise NotImplementedError
+
+    def signup(self):
+        raise NotImplementedError
+
+    def logout(self):
         raise NotImplementedError
