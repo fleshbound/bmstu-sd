884603c4994a8969f0fe8d7d13040447f99badc0
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-08-28 12:04:08 +0300

    add console select

--- internal/petowo/console/tech/utils/lang/impl/rulang.py
+++ internal/petowo/console/tech/utils/lang/impl/rulang.py
@@ -4,7 +4,8 @@ from internal.petowo.console.tech.utils.lang.langmodel import LanguageModel
 class RuLanguageModel(LanguageModel):
     no_empty_field = 'Ввод не может быть пустым'
     yes = 'Да'
-    input_incorrect = 'Некорректный ввод'
+    input_invalid = 'Некорректный ввод'
     question_year = 'Введите год: '
     question_month = 'Введите месяц: '
     question_day = 'Введите день: '
+    auth_token_expired = 'Время сессии истекло'
