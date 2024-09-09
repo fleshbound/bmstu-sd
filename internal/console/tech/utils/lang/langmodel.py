9194f9a2002712dc3e31196845027e91f58f32b9
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-09-09 19:09:02 +0300

    fix: project struct

--- internal/console/tech/utils/lang/langmodel.py
+++ internal/console/tech/utils/lang/langmodel.py
@@ -4,14 +4,8 @@ from pydantic import BaseModel
 class LanguageModel(BaseModel):
     no_empty_field: str
     yes: str
-    input_invalid: str
+    input_incorrect: str
     question_year: str
     question_month: str
     question_day: str
-    auth_token_expired: str
-    get_animals_empty_result: str
-    question_animal_name: str
-    out_question_animal_name: str
-    question_animal_breed_id: str
-    out_question_animal_breed_id: str
-    cancel_input: str
+
