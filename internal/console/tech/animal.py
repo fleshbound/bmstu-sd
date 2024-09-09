fcb8537ae5d6f48a2df80aa5bd2d8b99e40a4614
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-09-09 19:09:02 +0300

    fix: project struct

--- internal/console/tech/animal.py
+++ internal/console/tech/animal.py
@@ -1,9 +1,5 @@
-from internal.console.tech.utils.lang.langmodel import LanguageModel
-from internal.src.core.animal.service.animal import IAnimalService
-from internal.src.core.show.service.show import IShowService
-from internal.src.core.utils.exceptions import NotFoundRepoError
-
-from internal.src.core.utils.types import ID
+from core.animal.service.animal import IAnimalService
+from core.show.service.show import IShowService
 
 
 class AnimalHandler:
@@ -12,24 +8,16 @@ class AnimalHandler:
 
     def __init__(self,
                  animal_service: IAnimalService,
-                 show_service: IShowService,
-                 lang_model: LanguageModel):
+                 show_service: IShowService):
         self.animal_service = animal_service
         self.show_service = show_service
-        self.lang_model = lang_model
 
     def delete_animal(self):
-        raise NotImplementedError
+        raise
 
     def create_animal(self):
         raise NotImplementedError
 
-    def get_animals_by_user_id(self, user_id: ID):
-        try:
-            res = self.animal_service.get_by_user_id(user_id)
-        except NotFoundRepoError:
-            print(self.lang_model.get_animals_empty_result)
-        else:
-            for animal in res:
-
+    def get_animals_all(self):
+        raise
 
