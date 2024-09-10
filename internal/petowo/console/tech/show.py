6be7dcac3c2970ba1f71a118f56f50e1caaf54e8
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-09-10 20:14:34 +0300

    fix: merge deleted files

---
+++ internal/petowo/console/tech/show.py
@@ -0,0 +1,31 @@
+class ShowHandler:
+    def score_animal(self):
+        raise NotImplementedError
+
+    def create_show(self):
+        raise NotImplementedError
+
+    def start_show(self):
+        raise NotImplementedError
+
+    def stop_show(self):
+        raise NotImplementedError
+
+    def register_animal(self):
+        raise NotImplementedError
+
+    def unregister_animal(self):
+        raise NotImplementedError
+
+    def register_user(self):
+        raise NotImplementedError
+
+    def unregister_user(self):
+        raise NotImplementedError
+
+    def get_shows_all(self):
+        raise NotImplementedError
+
+    def get_show_result(self):
+        raise NotImplementedError
+    
