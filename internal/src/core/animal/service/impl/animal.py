24427a4746669aade32a3be5dbe2db65784b233d
Valeria Avdeykina <avp21u792@student.bmstu.ru>
2024-09-24 13:04:22 +0300

    start logging

--- internal/src/core/animal/service/impl/animal.py
+++ internal/src/core/animal/service/impl/animal.py
@@ -12,7 +12,7 @@ from core.show.service.animalshow import IAnimalShowService
 from core.show.service.show import IShowService
 from core.utils.exceptions import NotFoundRepoError, DeleteAnimalStartedShowError
 from core.utils.types import ID
-
+import logging
 from core.show.repository.show import IShowRepository
 
 
@@ -35,14 +35,18 @@ class AnimalService(IAnimalService):
         return records
 
     def delete(self, animal_id: ID) -> AnimalSchemaDelete:
+        logging.info(f'delete animal id={animal_id.value}')
+
         if not (animal_registration_records := self.get_animal_registration_records(animal_id)):
             self.animal_repo.delete(animal_id.value)
+            logging.info(f'success delete animal id={animal_id.value}')
             return AnimalSchemaDelete(id=animal_id)
 
         shows = []
         for record in animal_registration_records:
             cur_show = self.show_repo.get_by_id(record.show_id.value)
             if cur_show.status == ShowStatus.started:
+                logging.warning(f'error delete animal id={animal_id.value}: show status is started')
                 raise DeleteAnimalStartedShowError(animal_id=animal_id)
             shows.append(cur_show)
 
@@ -53,19 +57,24 @@ class AnimalService(IAnimalService):
         return AnimalSchemaDelete(id=animal_id)
 
     def create(self, create_animal: AnimalSchemaCreate) -> AnimalSchema:
+        logging.info(f'create animal (user_id={create_animal.user_id.value})')
         new_animal = AnimalSchema.from_create(create_animal)
         return self.animal_repo.create(new_animal)
 
     def update(self, update_animal: AnimalSchemaUpdate) -> AnimalSchema:
+        logging.info(f'update animal={update_animal.id.value}')
         cur_animal = self.animal_repo.get_by_id(update_animal.id.value)
         cur_animal = cur_animal.from_update(update_animal)
         return self.animal_repo.update(cur_animal)
 
     def get_all(self, skip: NonNegativeInt = 0, limit: PositiveInt = 100) -> List[AnimalSchema]:
+        logging.info(f'get all animals skip={skip}, limit={limit}')
         return self.animal_repo.get_all(skip, limit)
 
     def get_by_user_id(self, user_id: ID) -> List[AnimalSchema]:
+        logging.info(f'get animals by user_id={user_id.value}')
         return self.animal_repo.get_by_user_id(user_id.value)
 
     def get_by_id(self, animal_id: ID) -> AnimalSchema:
+        logging.info(f'get animal by id={animal_id.value}')
         return self.animal_repo.get_by_id(animal_id.value)
